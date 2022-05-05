8e9bc597805e7164b613644c383619c48a8d5888
Erin ODea <odea.erin@gmail.com>
2022-05-05 11:24:37 -0500

    make coffee hot, update coffee full

--- controllers/coffees.js
+++ controllers/coffees.js
@@ -43,9 +43,79 @@ function show(req, res) {
   })
 }
 
+function flipHot(req, res) {
+  Coffee.findById(req.params.id)
+  .then(coffee => {
+    coffee.hot = !coffee.hot
+    coffee.save()
+    .then(()=> {
+      res.redirect(`/coffees/${coffee._id}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/coffees')
+  })
+}
+
+function edit(req, res) {
+  Coffee.findById(req.params.id)
+  .then(coffee => {
+    res.render('coffees/edit', {
+      coffee,
+      title: "remake"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/coffees')
+  })
+}
+
+function update(req, res) {
+  Coffee.findById(req.params.id)
+  .then(coffee => {
+    if (coffee.barista.equals(req.user.profile._id)) {
+      req.body.hot = !!req.body.hot
+      coffee.updateOne(req.body, {new: true})
+      .then(()=> {
+        res.redirect(`/coffees/${coffee._id}`)
+      })
+    } else {
+      throw new Error ('🚫 Not authorized 🚫')
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/coffees`)
+  })
+}
+
+function deleteCoffee(req, res) {
+  Coffee.findById(req.params.id)
+  .then(coffee => {
+    if (coffee.barista.equals(req.user.profile._id)) {
+      coffee.delete()
+      .then(() => {
+        res.redirect('/coffees')
+      })
+    } else {
+      throw new Error ('🚫 Not authorized 🚫')
+    }   
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/coffees')
+  })
+}
+
 export {
   index,
   create,
   show,
+  flipHot,
+  edit,
+  update,
+  deleteCoffee as delete,
 }
 
--- routes/coffees.js
+++ routes/coffees.js
@@ -6,6 +6,10 @@ import { isLoggedIn } from '../middleware/middleware.js'
 router.get('/', coffeesCtrl.index)
 router.post('/', isLoggedIn, coffeesCtrl.create)
 router.get('/:id', coffeesCtrl.show)
+router.patch('/:id/flip-hot', isLoggedIn, coffeesCtrl.flipHot)
+router.get('/:id/edit', isLoggedIn, coffeesCtrl.edit)
+router.put('/:id', isLoggedIn, coffeesCtrl.update)
+router.delete('/:id', isLoggedIn, coffeesCtrl.delete)
 
 export {
   router
